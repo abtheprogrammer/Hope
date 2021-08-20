cd2247e07f57c03096dd385cdb74a60fd7df6850
abtheprogrammer <rajab2000changomango@gmail.com>
2021-08-20 15:48:13 -0700

    Initialized project

---
+++ api/database/seeders/FruitSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Fruit;
+use Illuminate\Database\Seeder;
+
+class FruitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Fruit :: factory()->count(50)->create();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
